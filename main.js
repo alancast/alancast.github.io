627ab4db9501d763f7510fcab2a66cafbebae8ea
Alex <alancast@umich.edu>
2015-03-26 15:25:53 -0400

    getting main.js working

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function change_content(s){
-    $.get(s, function(data) {
+    $.get(s, function (data) {
             $("#content").html(data);
+            console.log("hello");
         });
 }
 
-$("#resume-click").click(
-    function() {
+$("#resume-click").click(function (e) {
         change_content("Resume/index.html");
+        console.log("clicked on resume");
     });
