deb4e4a6e3fd8df4dc02e8405e46ab90a0e46ab4
Alex <alancast@umich.edu>
2015-03-26 16:02:19 -0400

    you got what I need

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-function change_content(s){
-    $.get(s, function (data) {
-            console.log(s);
-            $("#content").html(data);
-            console.log("hello");
-        });
+function change_content(s, e) {
+    $.get(s, function(data) {
+        $("#content").html(data);
+    });
+    $(".active").removeClass("active");
+    $(e.target).addClass("active");
 }
 
-$("#resume-click").click(function (e) {
-        change_content("Resume/index.html");
-        console.log("clicked on resume");
-    });
+$("#resume-click").click(function(e) {
+    change_content("Resume/resume.html", e);
+});
